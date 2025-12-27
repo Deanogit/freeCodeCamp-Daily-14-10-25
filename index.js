b2bdd6cd366519a1f2c10ebcb1ed37e253d897b5
Dean Howe <hideanhowe@gmail.com>
2025-12-27 12:05:28 +0000

    solution

--- index.js
+++ index.js
@@ -3,10 +3,29 @@
 
 // The pattern string can overlap in the first string. For example, "aaa" contains "aa" twice. The first two a's and the second two.
 
+// function count(text, pattern) {
+//   console.log(text, pattern);
+//   const regex = /`${pattern}`/;
+//   const arr = test.matchAll(regex);
+//   console.log(arr);
+//   // return text;
+// }
+
 function count(text, pattern) {
-  console.log(text, pattern);
-  const regex = /`${pattern}`/;
-  const arr = test.matchAll(regex);
-  console.log(arr);
+  let totalCount = 0;
+
+  for (let i = 0; i <= text.length - pattern.length; i++) {
+    // check if substring starting at this index matches the patter
+    if (text.slice(i, i + pattern.length) === pattern) {
+      totalCount++;
+    }
+  }
+
+  return totalCount;
+
+  // console.log(text, pattern)
+  // const regex = /`${pattern}`/;
+  // const arr = test.matchAll(regex)
+  // console.log(arr)
   // return text;
 }
