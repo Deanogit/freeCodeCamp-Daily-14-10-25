0cccb7a57e31fbef4b58b7a19ee439a15639061f
Dean Howe <hideanhowe@gmail.com>
2025-12-27 11:59:41 +0000

    testing

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 // Given two strings, determine how many times the second string appears in the first.
 
 // The pattern string can overlap in the first string. For example, "aaa" contains "aa" twice. The first two a's and the second two.
+
+function count(text, pattern) {
+  console.log(text, pattern);
+  const regex = /`${pattern}`/;
+  const arr = test.matchAll(regex);
+  console.log(arr);
+  // return text;
+}
